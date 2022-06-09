b643b780218846f913a56bf7114d513b7ace6bfd
Ilian Petrov <Ilian.Petrov@digitall.com>
2022-06-09 18:51:07 +0300

    test qr scanner libs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import QRScanner from "./components/QRScanner";
+import QRCode from "react-qr-code";
+
+const exampleObj = {
+  id: "1",
+  type: "item",
+  details: {
+    meta: [],
+  },
+};
 
 function App() {
+  const codeData = JSON.stringify(exampleObj);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <QRScanner />
+        <QRCode value={codeData} />
       </header>
     </div>
   );
